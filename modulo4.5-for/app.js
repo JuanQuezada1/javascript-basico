698ecc524402f8dfe7ebbb22ea32af31ab76340f
JuanQuezada1 <98851871+JuanQuezada1@users.noreply.github.com>
2022-06-30 16:46:07 -0400

    Update app.js

--- modulo4.5-for/app.js
+++ modulo4.5-for/app.js
@@ -14,7 +14,7 @@ let limite = 5;
 
 for (let i = 1; i <= limite; i++){
   let resultado = base * i;
-  console.log(base + ' x ' + limite + ' = ' + resultado);
+  console.log(base + ' x ' + i + ' = ' + resultado);
 }
 
 
