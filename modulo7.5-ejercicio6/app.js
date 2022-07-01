17eefa7543f9b4d14724cc6a4cddc37f0cfaec0c
JuanQuezada1 <98851871+JuanQuezada1@users.noreply.github.com>
2022-06-30 21:08:45 -0400

    Update app.js

--- modulo7.5-ejercicio6/app.js
+++ modulo7.5-ejercicio6/app.js
@@ -1,34 +1,22 @@
 /*
       EJERCICIO:
-
    Realice un algoritmo que permita la impresion en consola de la siguiente tabla:
-
    1  2  3  4  5 
    2  4  6  8  10
    3  6  9  12 15 
    4  8  12 16 20
    5  10 15 20 25
-
    Se debe hacer con ciclo FOR
 -------------------------------------------------------------------------------
-
 */
 
-// Base es igual a la tabla que deseamos
-let base   = 1;
-
-//largo de la tabla que deseamos
-let largo = 5;
-
-// ancho de la tabla
-let ancho = 5;
-
-for (let i = 1; i <= largo; i++){
-   let resultado = base * i;
-   console.log(base + ' x ' + i + ' = ' + resultado);
- }
- 
-
+for (let i = 1; i <= 5; i++){
+   let fila = '';
+   for (let b = 1; b <= 5; b++){
+    fila += `${ b * i}    `;
+   }
+   console.log(fila);
+}
 
 
 
